9dc17298a3531ddad9bcba491324da28c86479bd
Sachiya999 <spsd49@gmail.com>
2023-04-22 15:11:17 +0530

    week_9 control

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+
+$config = [
+    'db_host' => 'localhost',
+    'db_port' => '3306',
+    'db_name' => 'posts',
+    'db_username' => 'root',
+    'db_password' => 'Sachiya_999@',
+    'site_url' => 'http://localhost:8080'
+];
+
+foreach($config as $key => $value){
+    $GLOBALS[$key] = $value;
+}
